3d8490af51a38e12f70788ce8dd7f4ff47a693ab
elhananperelprivate <eliperel20@gmail.com>
2023-03-03 18:15:16 +0200

    try

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
     const port = parseInt(process.env.PORT) || 3000;
-    const server = await app.listen(port, () => {
-        const address = server.address();
-        const serverUrl = typeof address === 'string' ? address : `http://${address.address}:${address.port}`;
-        console.log(`Application is running on: ${serverUrl}`);
-    });
+    await app.listen(port);
 }
 
 bootstrap();
